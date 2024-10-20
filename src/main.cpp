5f07cb91f4347601400b5153e1dbaed64ac7cca7
Antonio Natusch <a2022111958@estudiantes.upsa.edu.bo>
2024-10-20 14:42:15 -0400

    got rid of hello world example

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
-#include "../include/HelloWorld.h"
 
 int main() {
-    auto hw = new HelloWorld();
-    std::cout<<"The sum of 2 and 3 is: "<<hw->Sum(2,3)<<"\n";
     return 0;
 }
